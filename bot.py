037758785d35ffd5ca75430046776c4874beb670
singh876101 <sir830477@gmail.com>
2024-12-18 21:09:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - sonu
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram sonu
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -13,7 +13,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="TechVJ"),
+            plugins=dict(root="Sonu"),
             workers=50,
             sleep_threshold=10
         )
@@ -22,7 +22,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By sonu')
 
     async def stop(self, *args):
 
@@ -31,6 +31,6 @@ class Bot(Client):
 
 Bot().run()
 
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - sonu
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
